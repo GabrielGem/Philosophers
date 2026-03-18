ebc154278e51e320b81397a2a956845530d33dfa
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-03-18 09:51:15 -0300

    added: custom sleep function for precision

---
+++ srcs/ft_usleep.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_usleep.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 15:02:35 by gabrgarc          #+#    #+#             */
+/*   Updated: 2026/03/16 16:38:36 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static long	get_elapsed_time_us(struct timeval start, struct timeval end);
+
+void	ft_msleep(long msec)
+{
+	struct timeval	start;
+	struct timeval	current;
+	long			elapsed;
+	long			remain;
+	long			usec;
+
+	usec = msec * 1000;
+	gettimeofday(&start, NULL);
+	elapsed = 0;
+	while (elapsed < usec)
+	{
+		gettimeofday(&current, NULL);
+		elapsed = get_elapsed_time_us(start, current);
+		remain = usec - elapsed;
+		if (remain > 1000)
+			usleep(remain >> 1); // check bitwise operations
+	}
+}
+
+static long	get_elapsed_time_us(struct timeval start, struct timeval end)
+{
+	return ((end.tv_sec - start.tv_sec) * 1000000L + (end.tv_usec - start.tv_usec));
+}
