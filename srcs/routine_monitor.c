96a3260d03d802015f3bca204e7098c3c1095adc
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-03-21 19:29:17 -0300

    feature: Check if all the philosophers have already eaten.

--- srcs/routine_monitor.c
+++ srcs/routine_monitor.c
@@ -6,41 +6,75 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 14:38:36 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/03/18 18:11:47 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/03/21 19:09:27 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	check_death(t_data *table, int i);
+static int	all_satisfied(t_data *table);
+
 void	*routine_monitor(void *arg)
 {
-	long	now;
-	long	timestamp;
 	t_data	*table;
 	int		i;
 
 	table = (t_data *)arg;
-	i = 0;
 	while (1)
 	{
-		pthread_mutex_lock(&table->philos[i].meal_lock);
-		timestamp = get_current_time() - table->philos[i].last_meal;
-		//printf("timestamp: %ld\n current time: %ld\n", timestamp, get_current_time());
-		pthread_mutex_unlock(&table->philos[i].meal_lock);
-		if (timestamp > table->time_to_die)
+		i = 0;
+		while (i < table->number_of_philosophers)
 		{
-			pthread_mutex_lock(&table->dead_lock);
-			table->dead = 1;
-			pthread_mutex_unlock(&table->dead_lock);
-			pthread_mutex_lock(&table->log_lock);
-			now = get_current_time();
-			printf("%ld %d died\n", now - table->start_sim, table->philos[i].id);
-			pthread_mutex_unlock(&table->log_lock);
-			break ;
+			if (check_death(table, i))
+				return (NULL);
+			i++;
 		}
-		i++;
-		i = i % table->number_of_philosophers;
+		if (all_satisfied(table))
+			return (NULL);
 		usleep(500);
 	}
 	return (NULL);
+}
+
+static int	check_death(t_data *table, int i)
+{
+	long	timestamp;
+	int		meals;
+
+	pthread_mutex_lock(&table->philos[i].meal_lock);
+	timestamp = get_current_time() - table->philos[i].last_meal;
+	meals = table->philos[i].number_of_meals;
+	pthread_mutex_unlock(&table->philos[i].meal_lock);
+	if (timestamp > table->time_to_die
+		&& (table->number_of_times_each_philosopher_must_eat == -1
+			|| meals != table->number_of_times_each_philosopher_must_eat))
+	{
+		pthread_mutex_lock(&table->dead_lock);
+		table->dead = 1;
+		pthread_mutex_unlock(&table->dead_lock);
+		log_print(&table->philos[i], DIED);
+		return (1);
+	}
+	return (0);
+}
+
+static int	all_satisfied(t_data *table)
+{
+	int	i;
+	int	meals;
+
+	if (table->number_of_times_each_philosopher_must_eat == -1)
+		return (0);
+	i = 0;
+	while (i < table->number_of_times_each_philosopher_must_eat)
+	{
+		pthread_mutex_lock(&table->philos[i].meal_lock);
+		meals = table->philos[i].number_of_meals;
+		pthread_mutex_unlock(&table->philos[i].meal_lock);
+		if (meals < table->number_of_times_each_philosopher_must_eat)
+			return (0);
+		i++;
+	}
+	return (1);
 }
