d2acb2cc2470bc2153832f56dc1aa0e3f7c5e4e9
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-03-21 19:27:40 -0300

    added logic forks, refactored for readbility, added case 1 philo

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,139 +6,100 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 17:29:17 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/03/18 17:52:50 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/03/21 17:47:51 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 static int	pick_up_forks(t_philo *philo);
-static void	eat(t_philo *philo);
-static void	nap(t_philo *philo);
-static void	think(t_philo *philo);
-long	get_current_time(void);
+static int	eat(t_philo *philo);
+static int	nap(t_philo *philo);
+static int	think(t_philo *philo);
 
 void	*routine(void *arg)
 {
 	t_philo	*philo;
-	int		catch;
 
 	philo = (t_philo *)arg;
-	if (philo->id % 2 == 0)
-		ft_msleep(1);
+	if (philo->table->number_of_philosophers == 1)
+	{
+		log_print(philo, TAKEN_FORK);
+		ft_msleep(philo->table->time_to_die);
+		return (NULL);
+	}
 	while (1)
 	{
-		// TODO fix: deadlock all philo take left fork
-		pthread_mutex_lock(&philo->table->dead_lock);
-		if (philo->table->dead)
+		if (pick_up_forks(philo))
 			break ;
-		pthread_mutex_unlock(&philo->table->dead_lock);
-
-		catch = pick_up_forks(philo);
-		if (catch) // check if he pickup up fork for eating
-			eat(philo);
-		nap(philo);
-		think(philo);
+		if (eat(philo))
+			break ;
+		if (nap(philo))
+			break ;
+		if (think(philo))
+			break ;
 	}
 	return (NULL);
 }
 
 static int	pick_up_forks(t_philo *philo)
 {
-	long	now;
-	long	timestamp;
-
-	// If you fail to pick up one of the forks, it returns 0.	
-	pthread_mutex_lock(&philo->table->dead_lock);
-	if (philo->table->dead)
-		return (0);
-	pthread_mutex_unlock(&philo->table->dead_lock);
-
-	if (pthread_mutex_lock(&philo->table->forks[philo->id % philo->table->number_of_philosophers]) != 0)
-		return (0);
-	pthread_mutex_lock(&philo->table->log_lock);
-	now = get_current_time();
-	timestamp = now - philo->table->start_sim;
-	printf("%ld %d has taken a fork\n", timestamp, philo->id);
-	pthread_mutex_unlock(&philo->table->log_lock);
-
-	pthread_mutex_lock(&philo->table->dead_lock);
-	if (philo->table->dead)
-		return (0);
-	pthread_mutex_unlock(&philo->table->dead_lock);
-
-	// If you manage to grab the left one but not the right one, release the right one.
-	if (pthread_mutex_lock(&philo->table->forks[(philo->id + 1) % philo->table->number_of_philosophers]) != 0)
+	if (dead(philo->table))
+		return (1);
+	if (philo->id == philo->table->number_of_philosophers)
 	{
-		pthread_mutex_unlock(&philo->table->forks[philo->id % philo->table->number_of_philosophers]);
+		pthread_mutex_lock(philo->fork_left);
+		if (dead(philo->table))
+		{
+			pthread_mutex_unlock(philo->fork_left);
+			return (1);
+		}
+		log_print(philo, TAKEN_FORK);
+		pthread_mutex_lock(philo->fork_right);
+		log_print(philo, TAKEN_FORK);
 		return (0);
 	}
-	pthread_mutex_lock(&philo->table->log_lock);
-	now = get_current_time();
-	timestamp = now - philo->table->start_sim;
-	printf("%ld %d has taken a fork\n", timestamp, philo->id);
-	pthread_mutex_unlock(&philo->table->log_lock);
-	return (1);
+	pthread_mutex_lock(philo->fork_right);
+	log_print(philo, TAKEN_FORK);
+	if (dead(philo->table))
+	{
+		pthread_mutex_unlock(philo->fork_right);
+		return (1);
+	}
+	pthread_mutex_lock(philo->fork_left);
+	log_print(philo, TAKEN_FORK);
+	return (0);
 }
 
-static void	eat(t_philo *philo)
+static int	eat(t_philo *philo)
 {
-	long	now;
-	long	timestamp;
-
-	pthread_mutex_lock(&philo->table->dead_lock);
-	if (philo->table->dead)
-		return ;
-	pthread_mutex_unlock(&philo->table->dead_lock);
-
+	if (dead(philo->table))
+		return (1);
+	log_print(philo, EATING);
 	pthread_mutex_lock(&philo->meal_lock);
 	philo->last_meal = get_current_time();
+	philo->number_of_meals++;
 	pthread_mutex_unlock(&philo->meal_lock);
-
-	pthread_mutex_lock(&philo->table->log_lock);
-	now = get_current_time();
-	timestamp = now - philo->table->start_sim;
-	printf("%ld %d is eating\n", timestamp, philo->id);
-	pthread_mutex_unlock(&philo->table->log_lock);
-
 	ft_msleep(philo->table->time_to_eat);
-
-	pthread_mutex_unlock(&philo->table->forks[philo->id % philo->table->number_of_philosophers]);
-	pthread_mutex_unlock(&philo->table->forks[(philo->id + 1) % philo->table->number_of_philosophers]);
+	pthread_mutex_unlock(philo->fork_right);
+	pthread_mutex_unlock(philo->fork_left);
+	return (0);
 }
 
-static void	nap(t_philo *philo)
+static int	nap(t_philo *philo)
 {
-	long	now;
-	long	timestamp;
-
-	pthread_mutex_lock(&philo->table->dead_lock);
-	if (philo->table->dead)
-		return ;
-	pthread_mutex_unlock(&philo->table->dead_lock);
-
-	pthread_mutex_lock(&philo->table->log_lock);
-	now = get_current_time();
-	timestamp = now - philo->table->start_sim;
-	printf("%ld %d is sleeping\n", timestamp, philo->id);
-	pthread_mutex_unlock(&philo->table->log_lock);
+	if (dead(philo->table))
+		return (1);
+	log_print(philo, SLEEPING);
 	ft_msleep(philo->table->time_to_sleep);
+	return (0);
 }
 
-static void	think(t_philo *philo)
+static int	think(t_philo *philo)
 {
-	long	now;
-	long	timestamp;
-
-	pthread_mutex_lock(&philo->table->dead_lock);
-	if (philo->table->dead)
-		return ;
-	pthread_mutex_unlock(&philo->table->dead_lock);
-	// How much time should you spend thinking?
-	pthread_mutex_lock(&philo->table->log_lock);
-	now = get_current_time();
-	timestamp = now - philo->table->start_sim;
-	printf("%ld %d is thinking\n", timestamp, philo->id);
-	pthread_mutex_unlock(&philo->table->log_lock);
+	if (dead(philo->table))
+		return (1);
+	log_print(philo, THINKING);
 	ft_msleep(1);
+	return (0);
 }
